af76ef852d34890100901e61956de711dce92687
Dalton Wilson <eremyja@gmail.com>
2019-05-11 14:11:54 -1000

    changes for sudo

--- commands.sh
+++ commands.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 cp bash_alias ~/.bash_aliases
-apt update
-apt upgrade -y
-apt -y install $(cat package_list)
 wget -O atom-amd64.deb https://atom.io/download/deb
-dpkg -i atom-amd64.deb
+sudo apt update && apt upgrade -y && apt -y install $(cat package_list) && dpkg -i atom-amd64.deb
 cd /etc/ssh/
-dpkg-reconfigure openssh-server
-apt-updater
+sudo dpkg-reconfigure openssh-server && apt-updater
+
